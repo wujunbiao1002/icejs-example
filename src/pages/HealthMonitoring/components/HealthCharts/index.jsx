ba12348af860301801b3b6e2d31597f27a34c1dd
wujunbiao <wujunbiao@richstonedt.com>
2021-03-16 18:18:20 +0800

    feat: 增加监控随机数据

--- src/pages/HealthMonitoring/components/HealthCharts/index.jsx
+++ src/pages/HealthMonitoring/components/HealthCharts/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Icon,
   DatePicker,
@@ -8,15 +8,28 @@ import {
 import styles from './index.module.scss';
 import echarts from 'echarts';
 import { houseType, rate } from '@/static/js/echartsHealth';
+import { logger } from 'ice';
+import moment from 'moment';
 
+moment.locale('zh-cn');
 const { RangePicker } = DatePicker;
-let housePropertyInit;
-let rateInit;
+const heartRateChart = {
+  chart: null,
+  option: Object.assign({}, rate),
+};
+const bloodPressureChart = {
+  chart: null,
+  option: Object.assign({}, houseType),
+};
 
 const HealthCharts = (props) => {
+  const [startDate, setStartDate] = useState('2021-03-14');
+  const [endDate, setEndEndDate] = useState('2021-03-20');
+
   const { dataSource } = props;
   const resizeCharts = () => {
-    housePropertyInit.resize();
+    bloodPressureChart.chart.resize();
+    heartRateChart.chart.resize();
   };
 
   useEffect(() => {
@@ -25,11 +38,11 @@ const HealthCharts = (props) => {
   }, []);
 
   const loadChart = () => {
-    housePropertyInit = echarts.init(document.getElementById('pressure'));
-    housePropertyInit.setOption(houseType);
+    bloodPressureChart.chart = echarts.init(document.getElementById('pressure'));
+    bloodPressureChart.chart.setOption(bloodPressureChart.option);
     // 心率
-    rateInit = echarts.init(document.getElementById('rate'));
-    rateInit.setOption(rate);
+    heartRateChart.chart = echarts.init(document.getElementById('rate'));
+    heartRateChart.chart.setOption(heartRateChart.option);
   };
 
   const handleBack = () => {
@@ -50,8 +63,48 @@ const HealthCharts = (props) => {
       </div>
     );
   };
+  const onClickSearch = () => {
+    creatData();
+    bloodPressureChart.option.xAxis[0].data = ['2021-03-14', '2021-03-15', '2021-03-16', '2021-03-17', '2021-03-18', '2021-03-19', '2021-03-20'];
+    bloodPressureChart.option.series[0].data = [108, 432, 90, 555, 84, 888];
+    bloodPressureChart.option.series[1].data = [55, 300, 50, 400, 50, 200];
 
+    heartRateChart.option.xAxis[0].data = ['2021-03-14', '2021-03-15', '2021-03-16', '2021-03-17', '2021-03-18', '2021-03-19', '2021-03-20'];
+    heartRateChart.option.series[0].data = [108, 432, 90, 555, 84, 888];
 
+    bloodPressureChart.chart.setOption(bloodPressureChart.option);
+    heartRateChart.chart.setOption(heartRateChart.option);
+  };
+
+  const creatData = () => {
+    let _dt1 = new Date(startDate);
+    const _dt2 = new Date(endDate);
+    const dt1 = _dt1.getTime();
+    const dt2 = _dt2.getTime();
+    const day = parseInt(Math.abs(dt1 - dt2) / 1000 / 60 / 60 / 24);
+    const dateList = [startDate];
+    for (let i = 1; i <= day; i++) {
+      _dt1 = _dt1.setDate(_dt1.getDate() + 1);
+      _dt1 = new Date(_dt1);
+      logger.debug(formatDate(_dt1));
+      dateList.push(formatDate(_dt1));
+
+    }
+  };
+  const formatDate = function (date) {
+    const y = date.getFullYear();
+    let m = date.getMonth() + 1;
+    m = m < 10 ? '0' + m : m;
+    let d = date.getDate();
+    d = d < 10 ? ('0' + d) : d;
+    return y + '-' + m + '-' + d;
+  };
+  const onOkRangePicker = (val) => {
+    logger.debug(val[0].format('YYYY-MM-DD'));
+    logger.debug(val[1].format('YYYY-MM-DD'));
+    setStartDate(val[0].format('YYYY-MM-DD'));
+    setEndEndDate(val[1].format('YYYY-MM-DD'));
+  };
   return (
     <div>
       <div className={styles.healthContent}>
@@ -112,8 +165,8 @@ const HealthCharts = (props) => {
         <div className={styles.centerGap} />
         <div className={styles.rightContent}>
           <div className={styles.searchWrap}>
-            <RangePicker />
-            <Button type="primary">查询</Button>
+            <RangePicker onOk={onOkRangePicker} />
+            <Button type="primary" onClick={onClickSearch}>查询</Button>
           </div>
 
           <div className={styles.rightGap} />
